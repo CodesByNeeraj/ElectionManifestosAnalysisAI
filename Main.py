e9bb350220eafda641da107535496bb559d09e73
CodesByNeeraj <94336586+CodesByNeeraj@users.noreply.github.com>
2025-06-11 12:16:48 +0800

    feat: modified such that we load db only once

--- Main.py
+++ Main.py
@@ -1,11 +1,9 @@
 import streamlit as st
 import streamlit.components.v1 as components
 from evaluatemanifestos import evaluate_manifestos_with_overlap
-from createvectordatabase import create_db
+from load_db import load_db
 from langchain.chat_models import ChatOpenAI
 from secret_key import secret_key
-# Initialize OpenAI model
-llm = ChatOpenAI(openai_api_key=secret_key, temperature=0)
 
 def main():
   st.title("Singapore Elections Map - 2025")
@@ -26,21 +24,20 @@ def main():
   height = st.slider("Adjust map height", 400, 1200, 680)
   components.html(iframe_code, height=height)
   
-  query = st.text_input("Ask a question about the manifestos:")
+  query = st.text_input("Ask a question about GE 2025")
   
   if query: 
     
-  # Load the full manifesto data (no similarity search in this case)
-    db = create_db()
+  # Load the vector database
+    db = load_db()
     
     # Initialize the LLM (OpenAI model) for response generation
     llm = ChatOpenAI(openai_api_key=secret_key, temperature=0)
     
     # Process the query and evaluate manifestos by chunking
-    answer = evaluate_manifestos_with_overlap(query, db, llm,3)
+    answer = evaluate_manifestos_with_overlap(query, db, llm,10)
     st.write(answer)
    
 if __name__ == "__main__":
     main()
    
-  
